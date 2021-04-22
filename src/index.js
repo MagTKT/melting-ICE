bbe4eeaa7c28603d11d3f8b437d01a85cfe70de9
magali gosselin <magali@wertkt.com>
2021-04-22 18:51:28 +0200

    fix bug other

--- src/index.js
+++ src/index.js
@@ -2,27 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
-
 // styles
-import "assets/css/bootstrap.min.css";
-import "assets/css/paper-kit.css?v=1.2.0";
-import "assets/demo/demo.css?v=1.2.0";
-
+import "bootstrap/scss/bootstrap.scss";
+import "assets/scss/paper-kit.scss?v=1.3.0";
+import "assets/demo/demo.css?v=1.3.0";
 // pages
 import Index from "views/Index.js";
 import InfoPage from "views/InfoPage.js";
 import ProfilePage from "views/ProfilePage.js";
 import ContactPage from "views/ContactPage.js";
 import GamePage from "views/GamePage.js";
-
 // others
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route 
-        path="/index" 
-        render={(props) => <Index {...props} />} />
+      <Route path="/index" render={(props) => <Index {...props} />} />
       <Route
         path="/info-page"
         render={(props) => <InfoPage {...props} />}
@@ -42,6 +37,5 @@ ReactDOM.render(
       <Redirect to="/index" />
     </Switch>
   </BrowserRouter>,
-  
   document.getElementById("root")
 );
