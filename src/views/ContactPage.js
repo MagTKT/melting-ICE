e950f8d4a02848a13d39c1e055426c8cfdc196d1
magali gosselin <magali@wertkt.com>
2021-04-24 21:35:14 +0200

    create form contact

--- src/views/ContactPage.js
+++ src/views/ContactPage.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 // reactstrap components
-import { Button, Card, Form, Input, Container, Row, Col } from "reactstrap";
+import { Container } from "reactstrap";
+import Contact from '../Contact/Contact.js';
 
 // core components
 import Navbar from "components/Navbars/IndexNavbar.js";
@@ -23,30 +24,8 @@ function ContactPage() {
           backgroundImage: "url(" + require("assets/img/iceland.jpg").default + ")",
         }}
       >
-        <div className="filter" />
         <Container>
-          <Row>
-            <Col className="ml-auto mr-auto" lg="4">
-              <Card className="card-register ml-auto mr-auto">
-                <h3 className="title mx-auto">Contact</h3>
-                <Form className="register-form">
-                  <label>Nom</label>
-                  <Input placeholder="Nom" type="nom" />
-                  <label>Prénom</label>
-                  <Input placeholder="Prénom" type="prénom" />
-                  <label>Adresse</label>
-                  <Input placeholder="Adresse" type="adresse" />
-                  <label>Email</label>
-                  <Input placeholder="Email" type="text" />
-                  <label>Message</label>
-                  <Input placeholder="Message" type="textarea" />
-                  <Button block className="btn-round" color="info">
-                    Envoyer
-                  </Button>
-                </Form>
-              </Card>
-            </Col>
-          </Row>
+        <Contact />
         </Container>
         <div className="footer register-footer text-center">
           <h6>
